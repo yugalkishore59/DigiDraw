37c5d350d0e7303f8639b44d6b8f7ecb11b08f4c
Yugal Kishore <yugalkishore59@gmail.com>
2023-07-31 19:45:44 +0530

    updated code (clean code)

--- DigiDraw/Assets/Scripts/FirebaseAndGPGS.cs
+++ DigiDraw/Assets/Scripts/FirebaseAndGPGS.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Analytics;
+using Firebase.Auth;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using UnityEngine.SocialPlatforms;
@@ -10,78 +11,111 @@ using System.Threading.Tasks;
 using TMPro;
 
 
-public class FirebaseAndGPGS : MonoBehaviour{
 
-    [SerializeField] TextMeshProUGUI txt;
+public class FirebaseAndGPGS : MonoBehaviour{
+    public static FirebaseAndGPGS Instance {get; private set;}
+
+    public TextMeshProUGUI logsTxt;
     string logs;
 
-    private void Start() {
+    [Header("Firebase")]
+    FirebaseApp firebaseApp;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    [SerializeField] bool isFireBaseReady = false;
+
+
+    [Header("GPGS")]
+    [SerializeField] bool isGooglePlaySignedIn = false;
+    string gpgsServerAuthCode;
+
+    [Header("Game")]
+    public string userName="Guest";
+    public string userId="0";
+    public int sketches=0;
+    public int rank=0;
+
+    private void Awake() {
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else Destroy(gameObject);
+
+        PlayGamesPlatform.Activate();
         InitializeFirebase();
-        InitializeGPGS();
+        if(isFireBaseReady) SignIntoGPGS();
     }
+
     private void Update() {
-        txt.text = logs;
+        logsTxt.text = logs;
     }
+
     void InitializeFirebase(){
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>{
-            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available){
+                FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+                firebaseApp = Firebase.FirebaseApp.DefaultInstance;
+                firebaseAuth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+                isFireBaseReady = true;
+            }else{
+                Debug.LogError(System.String.Format("Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                logs+= "Could not resolve all Firebase dependencies: "+dependencyStatus+"\n";
+            }
         });
     }
-    void InitializeGPGS(){
-        PlayGamesPlatform.Activate();
-        PlayGamesPlatform.Instance.Authenticate((success) =>{
-            if (success == SignInStatus.Success){
-                Debug.Log("Login with Google Play games successful.");
-                logs+="Login with Google Play games successful, ";
-                PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
-                {
-                    if(code == "" || code == null){
-                        logs+="did not get code from play games, ";
-                        return;
-                    }
-                    Debug.Log("Authorization code: " + code);
-                    logs+="Authorization code: " + code+", ";
-                    string Token = code;
-                    // This token serves as an example to be used for SignInWithGooglePlayGames
-                    AuthenticateFirebase(Token);
-                });
-            }
-            else
-            {
-                //Error = "Failed to retrieve Google play games authorization code";
-                Debug.Log("Login Unsuccessful");
-                logs+="Login Unsuccessful, ";
-            }
-        });
+    public void SignIntoGPGS(){
+        if(!isGooglePlaySignedIn){
+            PlayGamesPlatform.Instance.Authenticate((success) =>{
+                if (success == SignInStatus.Success){
+                    Debug.Log("Login with Google Play games successful.");
+                    logs+="Login with Google Play games successful.\n";
+
+                    PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>{
+                        gpgsServerAuthCode = code;
+                        if(gpgsServerAuthCode == "" || gpgsServerAuthCode == null){
+                            logs+="Could not get auth code from play games.\n";
+                            return;
+                        }
+                        AuthenticateFirebase(gpgsServerAuthCode);
+                        isGooglePlaySignedIn = true;
+                    });
+                }else{
+                    Debug.Log("Login Unsuccessful");
+                    logs+="Login Unsuccessful.\n";
+                }
+            });
+        }else{
+            Debug.Log("Already Signed into Google Play Games");
+            logs+="Already Signed into Google Play Games.\n";
+        }
     }
     void AuthenticateFirebase(string authCode){
-        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-        Firebase.Auth.Credential credential = Firebase.Auth.PlayGamesAuthProvider.GetCredential(authCode);
-        auth.SignInAndRetrieveDataWithCredentialAsync(credential).ContinueWith(task => {
+        Credential credential = Firebase.Auth.PlayGamesAuthProvider.GetCredential(authCode);
+        firebaseAuth.SignInAndRetrieveDataWithCredentialAsync(credential).ContinueWith(task => {
         if (task.IsCanceled) {
             Debug.LogError("SignInAndRetrieveDataWithCredentialAsync was canceled.");
-            logs+="SignInAndRetrieveDataWithCredentialAsync was canceled, ";
+            logs+="SignInAndRetrieveDataWithCredentialAsync was canceled\n";
             return;
         }
         if (task.IsFaulted) {
             Debug.LogError("SignInAndRetrieveDataWithCredentialAsync encountered an error: " + task.Exception);
-            logs+="SignInAndRetrieveDataWithCredentialAsync encountered an error: " + task.Exception+", ";
+            logs+="SignInAndRetrieveDataWithCredentialAsync encountered an error: " + task.Exception+"\n";
             return;
         }
 
-        Firebase.Auth.AuthResult result = task.Result;
-        Debug.LogFormat("User signed in successfully: {0} ({1})",
-            result.User.DisplayName, result.User.UserId);
-            logs+="User signed in successfully: "+ result.User.DisplayName+" "+ result.User.UserId+", ";
+        AuthResult result = task.Result;
+        Debug.LogFormat("User signed in successfully: {0}", result.User.DisplayName);
+            logs+="User signed in successfully: "+ result.User.DisplayName+"\n";
         });
-        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
-        if (user != null && user.IsValid()) {
-        string playerName = user.DisplayName;
+        firebaseUser = firebaseAuth.CurrentUser;
+        if (firebaseUser != null && firebaseUser.IsValid()) {
+        userName = firebaseUser.DisplayName;
 
         // The user's Id, unique to the Firebase project.
         // Do NOT use this value to authenticate with your backend server, if you
         // have one; use User.TokenAsync() instead.
-        string uid = user.UserId;
+        userId = firebaseUser.UserId;
         }
     }
 }
