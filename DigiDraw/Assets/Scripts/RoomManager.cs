0bc7c14becaeb6239d9775a7c7ee85e44ad42b18
Yugal Kishore <yugalkishore59@gmail.com>
2023-08-03 15:31:46 +0530

    update

--- DigiDraw/Assets/Scripts/RoomManager.cs
+++ DigiDraw/Assets/Scripts/RoomManager.cs
@@ -14,9 +14,11 @@ public class RoomManager : NetworkBehaviour{
     private static int maxWaitingTime = 5; //5sec
 
     public int gameMode = 0; //0 waiting, 1 guessing, 2 drawing
-    private int totalPlayers = 0; //clientIds size
-    private List<ulong> clientIds = new List<ulong>(GameManager.Instance.maxPlayers); //list holding client ids
-    private bool isLastRound = false;
+    //private int totalPlayers = 0; //clientIds size
+    private List<ulong> clientIdQueue = new List<ulong>(GameManager.Instance.maxPlayers); //list holding client ids
+    private int currentArtistIndex = 0;
+    private int round = 1; 
+    private int maxRounds = 2; //max 2 rounds per game
 
     private NetworkVariable<ulong> currentArtist = new NetworkVariable<ulong>(); //who is drawing now
     private NetworkVariable<int> timer = new NetworkVariable<int>();
@@ -31,14 +33,15 @@ public class RoomManager : NetworkBehaviour{
 
         if(GameManager.Instance.isLobbyHost){
             NetworkManager.Singleton.StartHost();
-            isWaiting.Value = false;
-            totalPlayers=1;
+            isWaiting.Value = true;
+            //totalPlayers=1;
             currentArtist.Value = OwnerClientId;
+            currentArtistIndex = 0;
             timer.Value = 0;
-            clientIds.Add(OwnerClientId);
+            clientIdQueue.Add(OwnerClientId);
         }else{
             NetworkManager.Singleton.StartClient();
-            GetQueueServerRpc(OwnerClientId);
+            AddMeToQueueServerRpc(OwnerClientId);
             if(!isWaiting.Value){ // auto switch to guessing mode if not waiting
                 gameMode=1;
                 //set gamemode to 1
@@ -47,57 +50,63 @@ public class RoomManager : NetworkBehaviour{
     }
 
     [ServerRpc]
-    void GetQueueServerRpc(ulong _clientId){
-        clientIds.Add(_clientId);
-        totalPlayers=clientIds.Count;
+    void AddMeToQueueServerRpc(ulong _clientId){
+        clientIdQueue.Add(_clientId);
+        //totalPlayers=clientIdQueue.Count;
     }
 
     private void Update() {
 
+        if(isWaiting.Value){
+            if(gameMode != 0){
+                //switch to waiting mode
+                gameMode = 0;
+            }    
+        }
+
         //below this, only host can go
         if(!IsHost) return;
-        if(totalPlayers<2){
+        if(clientIdQueue.Count<2){
             //timer.Value=0; ?
             //switch to witing mode and send waiting message
             //set everyone in waiting mode
-            gameMode = 0;
+            if(!isWaiting.Value){
+                isWaiting.Value =true;
+            }
             //return();
         }else if(timer.Value <=0){
-            //switch gamemode accordingly
-            /*if(!isWaiting.Value){                                //BUG HERE DO NOT RUN GAME
-                ChangeGameModeClientRpc();
-            }*/
+            if(!isWaiting.Value){
+                //switch gamemode accordingly and set timer to max timer
+                if(round<=maxRounds){
+                    currentArtist.Value = clientIdQueue[currentArtistIndex];
+                    currentArtistIndex++;
+                    if(currentArtistIndex>=clientIdQueue.Count){
+                        currentArtistIndex=0;
+                        round++;
+                    }
+                    timer.Value = maxDrawingTime;
+                    ChangeGameModeClientRpc();
+                }else{
+                    isWaiting.Value = true;
+                    StartCoroutine(TimerCoroutine(maxWaitingTime));
+                    //send results
+                    isWaiting.Value = false;
+                }
+            }
         }else{
             timer.Value-=(int)Time.deltaTime;
         }
     }
 
     [ClientRpc]
-    public void ChangeGameModeClientRpc(){
-        if(isLastRound){ //was it last round
-                ChangeToWaitingModeServerRpc();
-                gameMode=0;
-                //send results
-        }else if(currentArtist.Value == OwnerClientId){
+    void ChangeGameModeClientRpc(){
+        if(currentArtist.Value == OwnerClientId){
+            //load drawing mode
             gameMode = 2;
         }else{
-            gameMode = 1;
+            //load guessing mode
+            gameMode =1;
         }
-        //set tettings accordingly
-
-    }
-
-    [ServerRpc]
-    void ChangeToWaitingModeServerRpc(){
-        isWaiting.Value =true;
-        ChangeGameModeClientRpc();
-        StartCoroutine(TimerCoroutine(maxWaitingTime));
-        isWaiting.Value = false;
-    }
-
-    [ClientRpc]
-    void ChangeToWaitingModeClientRpc(){
-        //set settings accordingly
     }
 
     private IEnumerator TimerCoroutine(int timerDuration){
